a9860eaf8503a5541b59849c6d407cd3bb8abf31
Jurgens Smit <contact@jurgenssmit.com>
2013-05-22 18:54:20 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smapper::Application.config.secret_token = 'cf91a25ca26236c8b2c0546509d290477ebeef1e99cff0c8e1a2941ab104df3a6a1709e00e39ddd80342bd12b9e9cc5f328988604a385a438232f691900c0619'
